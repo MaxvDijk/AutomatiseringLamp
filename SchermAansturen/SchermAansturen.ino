c564cd757433b22bfacf57777900f7e6514a379e
MaxvDijk <maxingovd@gmail.com>
2023-12-16 10:47:17 +0100

    Er komt een balletje in het scherm wat van kleur veranderd als de motion sensor iets detecteerd
    probleem 1: tekst wordt over elkaar heen geplakt.
    
    probleem 2: het moet zo geprogrammeerd worden dat het aanblijft tot er nogmaals een signaal gemeten wordt.

--- SchermAansturen/SchermAansturen.ino
+++ SchermAansturen/SchermAansturen.ino
@@ -8,25 +8,50 @@
 #define TFT_RST        8 
 #define TFT_DC         9
 
+int const SENSOR_PIN = 7;
+boolean objectDetect = false;
+
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST); // pins worden gezet
 
 void setup() {
+
+  pinMode (SENSOR_PIN, INPUT); 
+
   Serial.begin(9600);
 
   tft.initR(INITR_GREENTAB);          // Init ST7735S chip, Green tab
 
   tft.fillScreen(ST77XX_BLACK);       // Kleur scherm
-  testdrawtext("TEST", ST77XX_WHITE); // Text, kleur tekst
+   // Text, kleur tekst
   delay(1000);
+  tft.drawCircle(30,  10, 10, ST77XX_WHITE);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  tft.invertDisplay(true);
-  delay(500);
-  tft.invertDisplay(false);
-  delay(500);
+
+  if ( digitalRead( SENSOR_PIN))        // If detector Output is HIGH, there is no object
+  {
+    if (objectDetect == true) {         // If we had previously detected an object
+      objectDetect = false;
+      delay(10);
+      tft.fillCircle(30, 10, 10, ST77XX_RED);             // Set flag to no object detected
+      tft.setCursor(30, 30);
+      tft.println("TEST");
+        
+    }
+  }
+  else                                      // If the Output is LOW, there is an object detected
+  {
+    objectDetect = true;                    // Set flag
+    delay(10);
+    tft.fillCircle(30, 10, 10, ST77XX_GREEN);        // turn on LED to show the detection.
+    tft.setCursor(30, 30);
+    tft.println("object detected");
+    Serial.println("Object Detected");      // Also send detection to the serial monitor window
+  }
+  delay(1000);
 
 }
 
@@ -35,4 +60,11 @@ void testdrawtext(char *text, uint16_t color) { //functie om tekst op het scherm
   tft.setTextColor(color);
   tft.setTextWrap(true);
   tft.print(text);
+}
+void testdrawcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
+    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
+      tft.drawCircle(x, y, radius, color);
+    }
+  }
 }
