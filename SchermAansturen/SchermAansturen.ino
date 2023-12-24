7c18c03bb043e86041ddfea944318417f1e7c581
MaxvDijk <maxingovd@gmail.com>
2023-12-24 12:48:14 +0100

    Nieuw scherm wordt gebruik en geeft nu "Object Detected" aan wanneer er iets voor de motion sensor is En "Clear" als er niks voor is

--- SchermAansturen/SchermAansturen.ino
+++ SchermAansturen/SchermAansturen.ino
@@ -1,30 +1,32 @@
 
-
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define TFT_CS        10
-#define TFT_RST        8 
-#define TFT_DC         9
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3c //Screen adress
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 int const SENSOR_PIN = 7;
 boolean objectDetect = false;
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST); // pins worden gezet
+
 
 void setup() {
-
-  pinMode (SENSOR_PIN, INPUT); 
-
   Serial.begin(9600);
 
-  tft.initR(INITR_GREENTAB);          // Init ST7735S chip, Green tab
-
-  tft.fillScreen(ST77XX_BLACK);       // Kleur scherm
-   // Text, kleur tekst
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  pinMode (SENSOR_PIN, INPUT); 
   delay(1000);
-  tft.drawCircle(30,  10, 10, ST77XX_WHITE);
+
 
 }
 
@@ -33,38 +35,56 @@ void loop() {
 
   if ( digitalRead( SENSOR_PIN))        // If detector Output is HIGH, there is no object
   {
+    display.clearDisplay();
     if (objectDetect == true) {         // If we had previously detected an object
       objectDetect = false;
       delay(10);
-      tft.fillCircle(30, 10, 10, ST77XX_RED);             // Set flag to no object detected
-      tft.setCursor(30, 30);
-      tft.println("TEST");
+      display.setTextSize(1);             // Normal 1:1 pixel scale
+      display.setTextColor(SSD1306_WHITE);        // Draw white text
+      display.setCursor(0,0);             // Start at top-left corner
+      display.println(F("Clear!"));
+      display.display();
         
     }
   }
   else                                      // If the Output is LOW, there is an object detected
   {
+    display.clearDisplay();
     objectDetect = true;                    // Set flag
     delay(10);
-    tft.fillCircle(30, 10, 10, ST77XX_GREEN);        // turn on LED to show the detection.
-    tft.setCursor(30, 30);
-    tft.println("object detected");
+    display.setTextSize(1);             // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE);        // Draw white text
+    display.setCursor(0,0);             // Start at top-left corner
+    display.println(F("Object Detected!"));
     Serial.println("Object Detected");      // Also send detection to the serial monitor window
+    display.display();
   }
   delay(1000);
 
 }
 
-void testdrawtext(char *text, uint16_t color) { //functie om tekst op het scherm te krijgen
-  tft.setCursor(0, 0);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
-}
-void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
-    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
-      tft.drawCircle(x, y, radius, color);
-    }
+
+void testdrawcircle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
   }
+
+  delay(2000);
+}
+
+void testfillcircle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=max(display.width(),display.height())/2; i>0; i-=3) {
+    // The INVERSE color is used so circles alternate white/black
+    display.fillCircle(display.width() / 2, display.height() / 2, i, SSD1306_INVERSE);
+    display.display(); // Update screen with each newly-drawn circle
+    delay(1);
+  }
+
+  delay(2000);
 }
