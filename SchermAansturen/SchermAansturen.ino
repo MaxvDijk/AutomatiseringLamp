c9250a296af1109d37ec8ac6d4cca29e7217ead7
MaxvDijk <maxingovd@gmail.com>
2024-01-07 12:47:42 +0100

    Functies aangemaakt voor tekst weergeven op scherm.
    Bijna alle code een beschrijving gegeven. En geprobeert er voor te zorgen het leesbaar te maken.

--- SchermAansturen/SchermAansturen.ino
+++ SchermAansturen/SchermAansturen.ino
@@ -1,4 +1,4 @@
-
+// Libraries
 #include <SPI.h>
 #include <Wire.h>
 #include <WiFiS3.h>
@@ -8,113 +8,70 @@
 #include <Adafruit_SSD1306.h>
 #include "env.h"
 
-
+// WiFiClient en een MQTTClient worden aangemaakt
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
-HttpClient client(wifiClient, IPBridge, HuePort);
+HttpClient http = HttpClient(wifiClient, IPBridge, HuePort);
 
-const char publishTopic[] = "Max/lightStatus";
-const char subscribeTopic[] = "Max/lightStatus";
-const char lampOff[] = "{\"on\": false}";
-const char lampOn[] = "{\"on\": true}";
+const char publishTopic[] = "Max/lightStatus";  // Hier wordt de topic gedefinieerd waarnaartoe ge published wordt
+const char subscribeTopic[] = "Max/lightStatus"; // Hier wordt gesubscribed op een top
+int const SENSOR_PIN = 7;  // Bewegings sensor pin wordt gedefinieerd 
+int const LED_PIN = 3; // Led lamp pin wordt gedefinieerd
+int ledState = LOW; // De huidige status van de ledlamp
+int lastSensorState; // De vorige status van de bewegings sensor
+int currentSensorState; // De huidige status van de bewegings sensor
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3c //Screen adress
+#define SCREEN_WIDTH 128 // OLED scherm breedte, in pixels
+#define SCREEN_HEIGHT 64 // OLED scherm hoogte, in pixels
+#define OLED_RESET     -1 
+#define SCREEN_ADDRESS 0x3c // scherm adress
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-int const SENSOR_PIN = 7;
-int const LED_PIN = 3;
-boolean objectDetect = false;
-
-
-int ledState = LOW;     // the current state of LED
-int lastSensorState;    // the previous state of button
-int currentSensorState; //currentSensorState
-
 
 void setup() {
   Serial.begin(9600);
   
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  pinMode(LED_PIN, OUTPUT); // Led licht wordt als output aangegeven 
+  pinMode (SENSOR_PIN, INPUT); // Motionsensor wordt als input aangegeven
+  currentSensorState = digitalRead(SENSOR_PIN); // Huidige sensor status wordt opgehaald door het lezen van de bewegings sensor
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { // scherm wordt gestart. Start het scherm niet dan komt hij in een loop
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for(;;); 
   }
-
-  pinMode(LED_PIN, OUTPUT); //Led light gets put as output
-  pinMode (SENSOR_PIN, INPUT); //Motionsensor gets put as input
-
-  currentSensorState = digitalRead(SENSOR_PIN);
-
-  //Startup process
-  display.setTextSize(1.5);
-  display.setCursor(32,32);             // Start at top-left corner
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.println("Welcome");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-  display.setTextSize(1.5);
-  display.setCursor(10,32);             // Start at top-left corner
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.print("Starting up");
-  display.display();
-  for(int16_t i = 0; i < 7; i++){ //Zorgt er voor dat de stipjes achter Starting up komen
-    display.print(".");
-    display.display();
-    delay(150);
-  }
-  delay(200);
-  display.clearDisplay();
-  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
-    display.display();
-    delay(40);
-  }
-  display.clearDisplay();
-
-  //Connecting to MQTT
-while (WiFi.begin(SSID, PASS) != WL_CONNECTED){
+  
+  // Hier probeert hij connect te maken met de mqtt server
+  while (WiFi.begin(SSID, PASS) != WL_CONNECTED){
   delay(5000);
 }  
+  display.clearDisplay(); // Cleart het scherm
+  Startup(); // Voert de functie startup uit waar hij het opstart scherm laat zien
 
 bool MQTTconnected = false;
-mqttClient.setUsernamePassword(MQTTUsername, MQTTPassword); // Gives login to connect to the MQTT 
+mqttClient.setUsernamePassword(MQTTUsername, MQTTPassword); // Geeft login gegevens om te verbinden met MQTT server
 while (!MQTTconnected) {
   if (!mqttClient.connect(MQTTURL, MQTTPort)){
-  Serial.println("connecting to MQTT....."); // Prints to serial 
-  display.setCursor(10,32);             // Start at top-left corner
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.print("Connecting to MQTT ");
-  display.display();
-  for(int16_t i = 0; i < 7; i++){ //Zorgt er voor dat de stipjes achter Starting up komen
-    display.print(".");
-    display.display();
-    delay(80);
-  }
+  Serial.println("Connecting to MQTT....."); // Print naar serial dat hij aan het verbinden is. 
+  text(1, 10, 32, 1, "Connecting to MQTT ") // Print tekst naar scherm
+  dots(7, 80); //Plaatst 7 punten met een delay van 80 achter de tekst
   delay(200);
   display.clearDisplay();
   } else {
   MQTTconnected = true;
     Serial.println("connected to MQTT"); // Prints to serial 
-    display.setCursor(10,32);             // Start at top-left corner
-    display.setTextColor(SSD1306_WHITE);        // Draw white text
-    display.print("connected to MQTT!");
-    display.display();
+    text(1, 10, 32, 1, "connected to MQTT!"); // Print tekst naar scherm
     delay(100);
+    display.clearDisplay();
     }
   }
-  display.clearDisplay();
   mqttClient.onMessage(onMqttMessage);
   mqttClient.subscribe(subscribeTopic);
 
 }
 
 void loop() {
-  lastSensorState    = currentSensorState;      // save the last state
-  currentSensorState = digitalRead(SENSOR_PIN); // read new state
+  lastSensorState    = currentSensorState;      // Slaat de laatste status op
+  currentSensorState = digitalRead(SENSOR_PIN); // Leest de nieuwe status
   mqttClient.poll();
 
   if(lastSensorState == HIGH && currentSensorState == LOW) {
@@ -125,10 +82,8 @@ void loop() {
     digitalWrite(LED_PIN, ledState);
     if(ledState == HIGH) {
       display.clearDisplay();
-      display.setCursor(10,5);             // Start at top-left corner
-      display.setTextColor(SSD1306_WHITE);        // Draw white text
-      display.println("Light: On");
-      display.fillCircle(3, 8, 3, SSD1306_WHITE);
+      text(1, 10, 5, 1, "Light: On"); // voert de functie uit die tekst op het scherm zet
+      display.fillCircle(3, 8, 3, SSD1306_WHITE); // Maakt een balletje voor de tekst 
       display.display();
 
       //For HueAPI
@@ -140,9 +95,7 @@ void loop() {
 
     }else {
       display.clearDisplay();
-      display.setCursor(10,5);             // Start at top-left corner
-      display.setTextColor(SSD1306_WHITE);        // Draw white text
-      display.println("Light: Off");
+      text(1, 10, 5, 1, "Light: Off"); // Voert de functie uit die tekst op het scherm zet
       display.drawCircle(3, 8, 3, SSD1306_WHITE);
       display.display();
 
@@ -155,20 +108,10 @@ void loop() {
     }
     
   }
-  delay(400);
+  delay(200);
 } 
 
-void testdrawcircle(void) {
-  display.clearDisplay();
 
-  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
 
 //Receives an MQTT message
 void onMqttMessage(int messageSize) {
@@ -187,14 +130,10 @@ while (mqttClient.available()) {
 
 }
 
-void controlHueLamp(bool turnOn) {
+void controlHueLamp(bool turnOn) { //deze functie zorgt voor de communicatie naar de API
   // Bouw de URL voor de PUT-aanvraag
   String url = "/api/" + String(key) + "/lights/2/state";
   String body = "{\"on\":" + String(turnOn ? "true" : "false") + "}";
-
-  // Maak een HTTP-client
-  WiFiClient wifiClient;
-  HttpClient http = HttpClient(wifiClient, IPBridge, HuePort);
 
   // Voer de PUT-aanvraag uit
   http.beginRequest();
@@ -216,16 +155,55 @@ void controlHueLamp(bool turnOn) {
   }
 }
 
-
-void testfillcircle(void) {
-  display.clearDisplay();
-
-  for(int16_t i=max(display.width(),display.height())/2; i>0; i-=3) {
-    // The INVERSE color is used so circles alternate white/black
-    display.fillCircle(display.width() / 2, display.height() / 2, i, SSD1306_INVERSE);
-    display.display(); // Update screen with each newly-drawn circle
-    delay(1);
-  }
-
-  delay(2000);
+void text(float Textsize, int X, int Y, int Colour, char* Text){ //Deze functie zorgt er voor dat ik makkelijk teskt kan plaatsen op het scherm
+  display.setTextSize(Textsize);       //Zet de grote van de tekst
+  display.setCursor(X,Y);              //Zet de cursor op de aangegeven pixels
+  if (Colour == 1)
+    display.setTextColor(SSD1306_WHITE);  //Maakt tekst wit
+  else if (Colour == 2)
+    display.setTextColor(SSD1306_BLACK); //Maakt tekst zwart
+  else
+    display.setTextColor(SSD1306_INVERSE); //Maakt de tekst een andere kleur dan de back ground. zwart of wit
+  display.println(Text); // Print de input
+  display.display();  // Zorgt ervoor dat het op het scherm zichtbaar wordt
 }
+
+void Startup(){ //hier zie je het opstart process van het scherm
+  text(2, 32, 32, 1 ,"Welcome");
+  delay(2000);
+  display.clearDisplay();
+  text(1.5, 10, 32, 1, "Starting up");
+  dots(7, 150);
+  delay(400);
+  display.clearDisplay();
+  testdrawcircle();
+  delay(400);
+  test2drawcircle();
+  delay(2000);
+  display.clearDisplay();
+}
+
+void dots(int16_t amountDots, int16_t Delay){
+    for(int16_t i = 0; i < amountDots; i++){ //Zorgt er voor dat de stipjes achter Starting up komen
+    display.print(".");
+    display.display();
+    delay(Delay);
+  }
+}
+
+void testdrawcircle(void) { //circles vormen van klein naar groot
+  display.clearDisplay();
+  for(int16_t i=0; i<max(display.width(),display.height()); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_INVERSE);
+    display.display();
+    delay(2);
+  }
+}
+void test2drawcircle(void) { //circles vormen van groot naar klein
+  for(int16_t i=128; i>0 ; i-=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_INVERSE);
+    display.display();
+    delay(2);
+  }
+}
+
